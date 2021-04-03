636672b8be46391ee079d5666deae3d295203b52
mikaelajoelle <74077372+mikaelajoelle@users.noreply.github.com>
2021-04-02 22:16:49 -0700

    fetching user data from API

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then( response => response.json() )
+    .then( users => this.setState( {monsters: users} ) )
+  }
+  // fetching from URL - taking the response and converting it into JSON format that JS can understand
+  // then taking users from response and set monsters to that array of users
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map( monster => <h1 key={ monster.id }> { monster.name } </h1> )
+        }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
